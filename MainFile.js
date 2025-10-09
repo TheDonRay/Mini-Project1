7e70369bb3e242471ce1f4b4a5e2bcf6ecc7d43f
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-10-09 08:56:52 -0400

    working on this commit

--- MainFile.js
+++ MainFile.js
@@ -3,3 +3,13 @@
 /*Goal for this project is to create something fun using CLI and what I've learned from JS function fundamentals etc */ 
 
 // TODO: Figure out how to get user input from terminal line in Javascript 
+const readline = require('readline'); 
+
+const rl = readline.createInterface({
+    input: process.stdin, 
+    output: process.stdout
+}); 
+
+rl.question('Welcome to CalmLine what are you stressed about?'), (UserResponse) => { 
+    console.log('You are being s)
+}
