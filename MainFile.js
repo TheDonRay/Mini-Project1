4b57582c25fd51b6c45bd2cd81b8b544410aeba4
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2025-10-10 12:27:01 -0400

    working on mini project

--- MainFile.js
+++ MainFile.js
@@ -1,48 +1,54 @@
-// Main file for my mini Javascript project / going over functions. 
+// Main file for my mini Javascript project / going over functions.
 
-/*Goal for this project is to create something fun using CLI and what I've learned from JS function fundamentals etc */ 
+/*Goal for this project is to create something fun using CLI and what I've learned from JS function fundamentals etc */
 
-// TODO: Figure out how to get user input from terminal line in Javascript  
-// this is the initialization of the readline here 
-const readline = require('readline'); 
+// TODO: Figure out how to get user input from terminal line in Javascript
+// this is the initialization of the readline here
+const readline = require("readline");
 
 const rl = readline.createInterface({
-    input: process.stdin, 
-    output: process.stdout
-}); 
+  input: process.stdin,
+  output: process.stdout,
+});
 
-//global variable here 
+//global variable here
 async function readlineUser() {
-
-rl.question('Welcome to CalmLine what are you stressed about?', (userresponse) => {  
-    userResponse = userresponse;  
-    //TODO: Handle error here   
-    if (!userResponse) { 
-        console.log('You did not enter anything');  
+  rl.question(
+    "Welcome to CalmLine what are you stressed about?",
+    (userresponse) => {
+      userResponse = userresponse;
+      //TODO: Handle error here
+      if (!userResponse) {
+        console.log("You did not enter anything");
         rl.close();
-    }
-    console.log(`You are being stressed from ${userresponse}`); 
+      }
+      console.log(`You are being stressed from ${userresponse}`);
 
-    //TODO: ask another question here about user stress level based of a number.
-    rl.question('On a scale of 1-10 how stressed are you?', (userRate) => {
-        const stressLevels = Number(userRate); 
+      //TODO: ask another question here about user stress level based of a number.
+      rl.question("On a scale of 1-10 how stressed are you?", (userRate) => {
+        const stressLevels = Number(userRate);
 
-        if (isNaN(stresslevels) || stressLevels < 1){ 
-            console.log('Invalid number please enter a number'); 
-        } else { 
-            // Todo call the function here  
-            StessLevel(stresslevels)
-        } 
-        rl.close(); 
-    }); 
-});  
-
+        if (isNaN(stresslevels) || stressLevels < 1) {
+          console.log("Invalid number please enter a number");
+        } else {
+          // Todo call the function here
+          StessLevel(stresslevels);
+        }
+        rl.close();
+      });
+    },
+  );
 }
-  
 
-function StressLevel(userStressLevels) { 
-    // handle some try and catch to check if user Responsed or not.  
-    
-} 
-// function is already called in the read line function 
-StressLevel(); 
+const Calmline = await readlineUser();
+// dont need to invoke the function because the function is already called in the readline functions 
+function StressLevel(userStressLevels) {
+  // handle some try and catch to check if user Responsed or not. 
+  try { 
+    if (userStressLevels < 5) { 
+        console.log("Seems like you're not that stressed ")
+    }
+  }
+}
+// function is already called in the read line function
+
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 I’m an aspiring Backend Engineer passionate about mastering JavaScript by building hands-on mini projects that strengthen my understanding of the fundamentals.
 
-For the first Mini-project of many, I’ve created a simple Command-Line Interface (CLI) tool that helps users manage and organize stressful situations they might be facing. It takes user input, processes it, and helps structure your thoughts in a more organized and mindful way — a small step toward combining tech and mental well-being. 
+For the first Mini-project of many, I’ve created a simple Command-Line Interface (CLI) tool that helps users manage and organize stressful situations they might be facing. It takes user input, processes it, and helps structure your thoughts in a more organized and mindful way — a small step toward combining tech and mental well-being.
